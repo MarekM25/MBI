4f18c7382fe2f539a65d133f999ee839d7c59756
MarekM25 <marekm2504@gmail.com>
2016-05-04 23:41:16 +0200

    Edit view, add buttons, keyup event.

--- app/js/app.js
+++ app/js/app.js
@@ -7,15 +7,13 @@ var app = angular.module("app",[]);
 app.controller("AppCtrl",function($scope,$http) {
     var app = this;
 	
-	
-	$scope.msg = 'not clicked';
-	
 	$scope.medianSearchAlgorithm = function() {
-        console.log($scope.iloscSekwencji);
         console.log($scope.dlugoscSekwencji);
         console.log($scope.sekwencje);
         medianSearch($scope.sekwencje,$scope.dlugoscSekwencji);
   }
+  
+}
 })
 
 // X - tablica ciągów sekwencji
